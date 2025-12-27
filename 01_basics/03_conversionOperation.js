f2abfe94af8ad243e7e33578c77c678958fb632b
vickysrivastav <vicky0shrivaatav@gmail.com>
2025-12-27 06:27:47 +0000

    conversionOperations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 33;
 
 let srtingNumber = String(someNumber);
-console.log(srtingNumber);
-console.log(typeof(someNumber));
+// console.log(srtingNumber);
+// console.log(typeof(someNumber));
+
+//**************************** Operations ***************************** */
+
+let value = 3;
+let negValue = -value; 
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 **3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+
+let str1 = "hello";
+let str2 = " vicky";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3; 
+// num1 = num2 = num3 = 2 + 2
+
+let gameconuter = 100;
+ ++gameconuter;
+ console.log(gameconuter);
+ 
+
+
+
+
+
