da93c7ada0700a2029bd384975c3705db53a1c14
vickysrivastav <vicky0shrivaatav@gmail.com>
2025-12-31 10:24:35 +0000

    Array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,38 @@
+//Array 
+const MyArr = [0, 1, 2, 3, 4, 5, ];
+const MyHeors = ["shaktiman", "naagraj"];
+
+const Myarr2 = new Array(6);
+// console.log(MyArr[1]);
+
+//Array methods
+// MyArr.push(6);
+// MyArr.push(7);
+// MyArr.pop();
+// MyArr.unshift(9);
+// MyArr.shift();
+// console.log(MyArr.includes(9));
+// console.log(MyArr.indexOf(5));
+
+// const NewArr = MyArr.join();
+
+// console.log(MyArr);
+// console.log(NewArr);
+
+//Slice, Splice
+console.log("A ", MyArr);
+
+const myn1 = MyArr.slice(1,3);
+
+console.log(myn1);
+
+console.log("B ", MyArr);
+
+const myn2 = MyArr.splice(1,3);
+console.log("C ", MyArr);
+console.log(myn2);
+
+
+
+
+
